589fc23b57cecb042da64f086f463878b2d000ab
Michael <dannte@gmail.com>
2020-02-20 22:14:22 +0200

    cleaned

--- src/components/OrderSender/SingleSellerSender.tsx
+++ src/components/OrderSender/SingleSellerSender.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Box, Button, TextField, Typography} from '@material-ui/core';
+import { Button, TextField, Typography} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import {singleSellerOrderToStringList, singleSellerOrderAsText} from './logic';
